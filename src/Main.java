8d031992272851b31bc35bb000b2b352a9616f6d
mrg <rgmengedoth@gmail.com>
2023-07-04 10:43:41 +0200

    Test change v5

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("Test upload 2 / change v2");
         System.out.println("Test upload 3 / change v3");
         System.out.println("Test upload 4 / change v4");
+        System.out.println("Test commit und dann erst GitHub push");
 
     }
 }
