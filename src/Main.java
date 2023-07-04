1dc9ae03cfe7ae6e518794282235e3b3813c9a18
mrg <rgmengedoth@gmail.com>
2023-07-04 10:34:16 +0200

    Test change v3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test upload 2 / change");
+        System.out.println("Test upload 3 / change v3");
     }
 }
